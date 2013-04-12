0ea46a4cc6fc4fdcf3caada0e54c6bdd092a3c95
Alexandros Sigaras <alex@sigaras.com>
2013-04-11 19:52:18 -0700

    Added Driver, Library, Console and WPF Samples

---
+++ 4. Demo Applications/OWI535.Test.Console/Program.cs
@@ -0,0 +1,39 @@
+﻿// (c) Copyright Alexandros Sigaras.
+// OWI 535 Robotic Arm Library
+// Test Console Application
+// Project Website: https://github.com/alexsigaras/OWI535-RoboticArm
+//
+// This source is subject to the Microsoft Public License (Ms-PL).
+// Please see http://go.microsoft.com/fwlink/?LinkID=131993 for details.
+// All other rights reserved.
+
+using System;
+using System.Threading;
+using OWI535.Library;
+
+
+namespace OWI535.Test.Console
+{
+    class Program
+    {
+        public static ArmController arm = new ArmController(4711, 0, 0);
+
+        static void Main(string[] args)
+        {
+            int miliseconds = 1000;
+            arm.ArmDown(miliseconds);
+            arm.ArmUp(miliseconds);
+            arm.RotateLeft(miliseconds);
+            arm.RotateRight(miliseconds);
+            arm.ClawClose(miliseconds);
+            arm.ClawDown(miliseconds);
+            arm.ClawOpen(miliseconds);
+            arm.ClawUp(miliseconds);
+            arm.ElbowDown(miliseconds);
+            arm.ElbowUp(miliseconds);
+            arm.LightOn();
+            Thread.Sleep(2000);
+            arm.LightOff();
+        }
+    }
+}
---
+++ 4. Demo Applications/OWI535.Test.WPF/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+using OWI535.Library;
+using OWI535.Test.WPF.Properties;
+
+namespace OWI535.Test.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public static readonly int VendorID = Settings.Default.VendorID;
+        public static readonly int ProductID = Settings.Default.ProductID;
+        public static readonly int ArmID = Settings.Default.ArmID;
+        public static ArmController arm = new ArmController(VendorID, ProductID, ArmID);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Light_Checked(object sender, RoutedEventArgs e)
+        {
+            arm.LightOn();
+        }
+
+        private void Light_Unchecked(object sender, RoutedEventArgs e)
+        {
+            arm.LightOff();
+        }
+
+        private void ClawUpBtn_Click(object sender, RoutedEventArgs e)
+        {
+            arm.ClawUp(getArmSeconds(SecondsTxtBox.Text));
+        }
+
+        private void ClawDownBtn_Click(object sender, RoutedEventArgs e)
+        {
+            arm.ClawDown(getArmSeconds(SecondsTxtBox.Text));
+        }
+
+        private void ElbowUpBtn_Click(object sender, RoutedEventArgs e)
+        {
+            arm.ElbowUp(getArmSeconds(SecondsTxtBox.Text));
+        }
+
+        private void ElbowownBtn_Click(object sender, RoutedEventArgs e)
+        {
+            arm.ElbowDown(getArmSeconds(SecondsTxtBox.Text));
+        }
+
+        private void ArmUpBtn_Click(object sender, RoutedEventArgs e)
+        {
+            arm.ArmUp(getArmSeconds(SecondsTxtBox.Text));
+        }
+
+        private void ArmDownBtn_Click(object sender, RoutedEventArgs e)
+        {
+            arm.ArmDown(getArmSeconds(SecondsTxtBox.Text));
+        }
+
+        private void ArmRotateLeftBtn_Click(object sender, RoutedEventArgs e)
+        {
+            arm.RotateLeft(getArmSeconds(SecondsTxtBox.Text));
+        }
+
+        private void ArmRotateRightBtn_Click(object sender, RoutedEventArgs e)
+        {
+            arm.RotateRight(getArmSeconds(SecondsTxtBox.Text));
+        }
+
+        private int getArmSeconds(string armsSecondsStringValue)
+        {
+            int seconds;
+            Int32.TryParse(armsSecondsStringValue, out seconds);
+            return (seconds * 1000);
+        }
+    }
+}
